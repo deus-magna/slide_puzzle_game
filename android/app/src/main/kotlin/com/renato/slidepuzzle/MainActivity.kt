ca52faba0c04d8d91f7fd8a646f2f740407a82ee
Renato <52421382+deus-magna@users.noreply.github.com>
2022-01-20 13:20:28 -0600

    First commit, initial project

---
+++ android/app/src/main/kotlin/com/renato/slidepuzzle/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.renato.slidepuzzle.slide_puzzle_game
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/presentation/views/initial_view.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class InitialView extends StatelessWidget {
+  const InitialView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('Slide Puzzle'),
+      ),
+    );
+  }
+}
