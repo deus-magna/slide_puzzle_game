bec23633450e6d101d150c7a8fe358905622e8d6
Renato <52421382+deus-magna@users.noreply.github.com>
2022-01-20 16:15:19 -0600

    Adding board

--- lib/presentation/views/initial_view.dart
+++ lib/presentation/views/initial_view.dart
@@ -5,10 +5,65 @@ class InitialView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Slide Puzzle'),
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            Header(),
+            PuzzleBoard(),
+            Menu(),
+          ],
+        ),
       ),
     );
+  }
+}
+
+class Header extends StatelessWidget {
+  const Header({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(child: Text('Slide Puzzle'));
+  }
+}
+
+class PuzzleBoard extends StatelessWidget {
+  PuzzleBoard({Key? key}) : super(key: key);
+
+  final numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+
+  @override
+  Widget build(BuildContext context) {
+    final height = MediaQuery.of(context).size.height;
+    return Container(
+      color: Colors.green,
+      height: height * 0.60,
+      child: Padding(
+        padding: const EdgeInsets.all(8),
+        child: GridView.builder(
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 3,
+            mainAxisSpacing: 5,
+            crossAxisSpacing: 5,
+          ),
+          itemCount: numbers.length,
+          itemBuilder: (context, index) {
+            return numbers[index] != 0
+                ? Container(color: Colors.amber)
+                : const SizedBox.shrink();
+          },
+        ),
+      ),
+    );
+  }
+}
+
+class Menu extends StatelessWidget {
+  const Menu({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
   }
 }
